428bdac2d621b3b8b267a652ddd12d67ad9d4e39
Christophe Savard <christophe_savard@hotmail.ca>
2017-10-25 22:25:03 -0400

    Create GameDriver

---
+++ Comp345_Risk/Drivers/GameDriver.cpp
@@ -0,0 +1,23 @@
+#include "GameDriver.h"
+
+GameDriver::GameDriver()
+{
+}
+
+GameDriver::~GameDriver()
+{
+}
+
+void GameDriver::run()
+{
+}
+
+string GameDriver::getOpeningMessage()
+{
+	return "Starting Game driver";
+}
+
+string GameDriver::getClosingMessage()
+{
+	return "Ending Game driver";
+}
